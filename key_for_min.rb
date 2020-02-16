590a4f4f49255b0195d2545c46952c313f8c16d4
Abdul Khan <akhan17@outlook.com>
2020-02-16 22:41:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,21 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+hash1 = { "one" => 3, "two" => 2, "three" => 1 }
+hash2 = { "one" => 1, "two" => 2, "three" => 3 }
 
+def key_for_min_value(name_hash)
+  while name_hash.length != 1
+    if name_hash.values[0] > name_hash.values[1]
+      name_hash.shift
+    else
+      name_hash.delete_at(2)
+    end
+  end
+  puts name_hash.values[0]
 end
+
+#puts hash1
+
+#puts hash1.length
+key_for_min_value(hash2)
